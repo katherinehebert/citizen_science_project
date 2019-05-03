cf2814471513a26e248c27fd7db44f3bd9794847
ABeardsell <andreanne.beardsell@gmail.com>
2019-05-03 16:02:41 -0400

    data set

--- functions/extract_iNaturalist.R
+++ functions/extract_iNaturalist.R
@@ -2,16 +2,64 @@
 
 # load iNaturalist library
 library(rinat)
+library(maps)
+library(mapdata)
+library(mapproj)
+library(ggmap)
 
 # bounding box for Canada
-CAN <- c(45,-78,51,-64)
+CAN <- c(45,-78,62,-60)
 
-# extract 10 results of birds in Canada bounding box
 iNat <- get_inat_obs(taxon_name = "Aves",
              year = 2018,
              bounds = CAN,
              geo=TRUE, # exclude non-georeferenced observations
-             maxresults = 10) # extract 10 just to test function
-iNat
+             maxresults = 50000)
+         
 
-inat_map(iNat, map = "can", subregion = ".", plot = TRUE)
+inat_map(iNat, map="world",subregion = ".", plot = TRUE)
+
+map("worldHires","Canada",xlim=c(-141,-53), ylim=c(40,85), col="gray90", fill=TRUE)
+
+
+####Extract datas from Inat
+CAN <- c(45,-78,62,-60)#Set boundaries
+data1 <- get_inat_obs(taxon_name = "Aves",
+             year = 2018,
+             bounds = CAN,
+             geo=TRUE, # exclude non-georeferenced observations
+             maxresults = 10000)
+inat_datas <- write.csv(data1, file = "inat_datas.csv")
+
+data2 <- get_inat_obs(taxon_name = "Aves",
+                      year = 2018,
+             bounds = CAN,
+             geo=TRUE, # exclude non-georeferenced observations
+             maxresults = 10)
+
+
+
+ 
+inatmap <- function(grpid){
+    CAN <- c(45,-78,62,-60)
+  data1= get_inat_obs(taxon_name = "Aves",
+             year = 2018,
+             bounds = CAN,
+             geo=TRUE, # exclude non-georeferenced observations
+             maxresults = 10000)
+  data1=data1[which(!is.na(data1$latitude)),]
+  map <-get_map(location =c(min(data1$longitude),
+                            min(data1$latitude),
+                            max(data1$longitude),
+                            max(data1$latitude)),
+                messaging = FALSE)
+  p <-ggplot()
+  p= ggmap(map)+geom_point(data=data1,
+                           aes(x=as.numeric(longitude),
+                               y=as.numeric(latitude)))
+  p
+}
+
+map1 <- inatmap("piou")
+dev.off()
+head(data1)
