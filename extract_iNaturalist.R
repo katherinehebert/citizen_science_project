04acf195cd15bed9f880f9ccf5b538e6ecd8aa90
ABeardsell <andreanne.beardsell@gmail.com>
2019-05-03 09:44:39 -0400

    new branch

--- extract_iNaturalist.R
+++ extract_iNaturalist.R
@@ -4,12 +4,14 @@
 library(rinat)
 
 # bounding box for Canada
-CAN <- c(38.44047,-125,40.86652,-121.837)
+CAN <- c(45,-78,51,-64)
 
 # extract 10 results of birds in Canada bounding box
 iNat <- get_inat_obs(taxon_name = "Aves",
              year = 2018,
              bounds = CAN,
+             geo=TRUE, # exclude non-georeferenced observations
              maxresults = 10) # extract 10 just to test function
+iNat
 
-
+inat_map(iNat, map = "can", subregion = ".", plot = TRUE)
