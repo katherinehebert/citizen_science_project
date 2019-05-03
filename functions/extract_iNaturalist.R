3529a63e4e75c78167e5da8e180e1c5c2c7a71ca
ABeardsell <andreanne.beardsell@gmail.com>
2019-05-03 16:46:53 -0400

    update on the extract_Inaturalist script

--- functions/extract_iNaturalist.R
+++ functions/extract_iNaturalist.R
@@ -7,6 +7,21 @@ library(mapdata)
 library(mapproj)
 library(ggmap)
 
+####Extract datas from Inat per month for 2017
+CAN <- c(45,-78,62,-60)#Set boundaries
+
+datalist <- list()
+for (i in 1:12) {
+    data1<- get_inat_obs(taxon_name = "Aves",
+             year = 2017,
+             bounds = CAN,
+             month = i,
+             geo=TRUE, # exclude non-georeferenced observations
+             maxresults = 10000)
+   datalist[[i]] <- data1
+}
+df_final1<- do.call(rbind, datalist)
+
 
 ####Extract datas from Inat per month for 2018
 CAN <- c(45,-78,62,-60)#Set boundaries
@@ -22,20 +37,15 @@ for (i in 1:12) {
    datalist[[i]] <- data1
 }
 
-df_final<- do.call(rbind, datalist)
+df_final2<- do.call(rbind, datalist)
+
+df_final <- rbind(df_final1,df_final2)
+str(df_final)
 inat_datas <- write.csv(df_final, file = "inat_datas_all.csv")
 
-## Only for February ##
-data2 <- get_inat_obs(taxon_name = "Aves",
-                      year = 2018,
-                      month=2,
-             bounds = CAN,
-             geo=TRUE, # exclude non-georeferenced observations
-             maxresults = 10000)
-str(data2)
- 
+## Generate a map of all observations
 inatmap <- function(grpid){
-  data1=data1[which(!is.na(data1$latitude)),]
+  data1=df_final[which(!is.na(df_final$latitude)),]
   map <-get_map(location =c(min(data1$longitude),
                             min(data1$latitude),
                             max(data1$longitude),
