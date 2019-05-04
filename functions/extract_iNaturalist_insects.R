c55b0f6d86ab4adc12e9a5c75ae713de72c16b03
Katherine Hebert <katherine.ahebert@gmail.com>
2019-05-04 11:55:07 -0400

    remove final structure exploration

--- functions/extract_iNaturalist_insects.R
+++ functions/extract_iNaturalist_insects.R
@@ -70,8 +70,7 @@ for (i in 1:12){
     datalist[[i]] <- data1
 }
 d18<- do.call(rbind, datalist)
+
 ## Obtain a data frame from the list
 df_final<- rbind(d14,d15,d16,d17,d18)
-str(df_final)
-df_final$datetime
 inat_datas <- write.csv(df_final, file = "inat_datas_insects.csv")
