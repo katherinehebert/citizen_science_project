8c880a0e70cc90d5e45e7d00f639fbc25f63c2d5
Renata Diaz <diaz.renatam@gmail.com>
2019-05-04 10:42:55 -0400

    Preliminary maps script using toy data

--- functions/match_cd.R
+++ functions/match_cd.R
@@ -5,10 +5,14 @@ can <- read_sf('data/can_census_divisions_shp/gcd_000b11a_e.shp')
 
 qu <- unique(can$PRNAME)[6]
 
-can_data <- can %>%
+can<- can %>%
   filter(PRNAME == qu) %>%
-  as.data.frame() %>%
-  select(-geometry)
+  mutate(CDUID = as.integer(CDUID))
 
 nhs_data <- read.csv('data/canada_economic_metrics/censusdivisions_econmetrics.csv', stringsAsFactors = F)
+
+nhs_data <- nhs_data %>%
+  select(-Prov_Name, -CD_Name, -Geo_Type, -GNR) %>%
+  rename(CDUID = Geo_Code)
+
 
