065babfac0c655579f2bfbae8c2a23e538c48c90
eliane <eliane.duche@gmail.com>
2019-05-04 09:41:44 -0400

    script extract_obs_area up to date

--- functions/extract_obs_area.R
+++ functions/extract_obs_area.R
@@ -1,21 +1,29 @@
-# Separate observations by county or postal code
+# Separate observations by regions
 
 library(sf)
+library(dplyr)
 
+# Import shapefile of census regions and subset it to keep quebec regions
 regions <-  read_sf("data/can_census_divisions_shp/gcd_000b11a_e.shp")
+regions$PRNAME <- gsub("Quebec / Qu\xe9bec", "Quebec", regions$PRNAME)
+regions_qc <- subset(regions, PRNAME == "Quebec")
+# Reproject layer to wgs84
+regions_qc <- st_transform(regions_qc, crs = 4326) %>%  subset(select = c("CDUID", "geometry"))
 
+# Import observations csv and transform to spatial points 
+obs <- read.csv(file = "data/inat/inat_datas.csv")
+obs_sf <- st_as_sf(obs, coords = c("longitude", "latitude"), crs = 4326) %>% subset(select = c("X", "scientific_name", "datetime", "user_login","id", "geometry")) 
 
-obs_sf <- st_as_sf(obs, coords = c("UTM_Easting", "UTM_Northing"), crs = 26717)
+# Get observations that fall in each different census region (the output dataframe has each association associated with the right census id)
+obs_region <- data.frame()
+for (i in 1:nrow(regions_qc)){
+  census_region <- regions_qc[i, ]
+  ID <- census_region$CDUID
+  obs <- st_join(census_region, obs_sf, join = st_intersects)
+  obs <- st_set_geometry(obs, NULL)
+  obs_region <- rbind(obs_region, obs)
+}
 
-subset(census, PRNAME == )
+# Calculate metrics that we want to explain with our models
 
-for (i in 1:length(nids_spp)){
-  nids_spp[[i]]$x <- nids_spp[[i]]$UTM_Easting
-  nids_spp[[i]]$y <- nids_spp[[i]]$UTM_Northing
-  species <- unique(nids_spp[[i]]$Species)
-  file <- paste(species, species, sep = "/")  %>% paste("nids_zone", sep = "_")  %>% paste("csv", sep = ".")
-  points_sf <- st_as_sf(nids_spp[[i]], coords = c("UTM_Easting", "UTM_Northing"), crs = 26717)
-  nids_zone <- st_join(points_sf, zone_etude, join = st_intersects) %>% subset(id == 1)
-  assign(paste("sfnids", species, sep = "_"), nids_zone)
-  write.csv(nids_zone, file = file)
-} 
+## 
