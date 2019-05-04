b42f8310813f69cd3e23f71620939a3ec9a38d9c
ABeardsell <andreanne.beardsell@gmail.com>
2019-05-04 10:42:15 -0400

    inat data added for multiple years

--- functions/extract_iNaturalist.R
+++ functions/extract_iNaturalist.R
@@ -1,59 +1,92 @@
 # Extracting iNaturalist observations for birds in Canada
 
-# load iNaturalist library
+# load iNaturalist library + map library
 library(rinat)
 library(maps)
 library(mapdata)
-library(mapproj)
 library(ggmap)
 
-####Extract datas from Inat per month for 2017
+####Extract datas from Inat per month for 2011 to 2018 per month.
 CAN <- c(45,-78,62,-60)#Set boundaries
 
+#datalist <- list()
+#for (i in 1:12){
+ #   year=i
+  #  for (j in 1:12) {
+   # data1<- get_inat_obs(taxon_name = "Aves",
+   #          year = year,
+    #         bounds = CAN,
+     #        month = j,
+      #       geo=TRUE, # exclude non-georeferenced observations
+       #      maxresults = 10000)}
+    #datalist[[i]] <- data1
+#}
+#2014
 datalist <- list()
-for (i in 1:12) {
+for (i in 1:12){
     data1<- get_inat_obs(taxon_name = "Aves",
-             year = 2017,
+             year = 2014,
              bounds = CAN,
              month = i,
              geo=TRUE, # exclude non-georeferenced observations
              maxresults = 10000)
-   datalist[[i]] <- data1
+    datalist[[i]] <- data1
 }
-df_final1<- do.call(rbind, datalist)
+d14<- do.call(rbind, datalist)
 
-
-####Extract datas from Inat per month for 2018
-CAN <- c(45,-78,62,-60)#Set boundaries
-
+#2015
 datalist <- list()
-for (i in 1:12) {
+for (i in 1:12){
     data1<- get_inat_obs(taxon_name = "Aves",
-             year = 2018,
+             year = 2015,
              bounds = CAN,
              month = i,
              geo=TRUE, # exclude non-georeferenced observations
              maxresults = 10000)
-   datalist[[i]] <- data1
+    datalist[[i]] <- data1
 }
-
-####Extract datas from Inat per month for 2016
+d15<- do.call(rbind, datalist)
+#2016
 datalist <- list()
-for (i in 1:12) {
+for (i in 1:12){
     data1<- get_inat_obs(taxon_name = "Aves",
              year = 2016,
              bounds = CAN,
              month = i,
              geo=TRUE, # exclude non-georeferenced observations
              maxresults = 10000)
-   datalist[[i]] <- data1
+    datalist[[i]] <- data1
 }
-
-df_final3<- do.call(rbind, datalist)
-
-df_final <- rbind(df_final1,df_final2,df_final3)
-str(df_final)
-inat_datas <- write.csv(df_final, file = "inat_datas_all.csv")
+d16<- do.call(rbind, datalist)
+#2017
+datalist <- list()
+for (i in 1:12){
+    data1<- get_inat_obs(taxon_name = "Aves",
+             year = 2017,
+             bounds = CAN,
+             month = i,
+             geo=TRUE, # exclude non-georeferenced observations
+             maxresults = 10000)
+    datalist[[i]] <- data1
+}
+d17<- do.call(rbind, datalist)
+#2018
+datalist <- list()
+for (i in 1:12){
+    data1<- get_inat_obs(taxon_name = "Aves",
+             year = 2018,
+             bounds = CAN,
+             month = i,
+             geo=TRUE, # exclude non-georeferenced observations
+             maxresults = 10000)
+    datalist[[i]] <- data1
+}
+d18<- do.call(rbind, datalist)
+## Obtain a data frame from the list
+df_final1<- rbind(d14,d15,d16,d17,d18)
+str(df_final1)
+df_final1$datetime
+inat_datas <- write.csv(df_final1, file = "inat_datas_all.csv")
 
 ## Generate a map of all observations
 inatmap <- function(grpid){
@@ -70,6 +103,8 @@ inatmap <- function(grpid){
   p
 }
 
-inatmap("piou")
+inatmap("beautiful_map")
+
+### Export the pdf.
 dev.copy2pdf(file="Datas_Inat.pdf")
 dev.off()
