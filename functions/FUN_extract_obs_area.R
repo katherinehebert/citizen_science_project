93870364e4dd84cf734dcc486d8fadca70b3e604
Katherine Hebert <katherine.ahebert@gmail.com>
2019-05-04 19:10:30 -0400

    added standard error in the nerdiness index

--- functions/FUN_extract_obs_area.R
+++ functions/FUN_extract_obs_area.R
@@ -35,7 +35,9 @@ mean_observer_effort <- obs_region %>%
   group_by(CDUID, user_login) %>% 
   summarise(nb_obs = n()) %>% 
   group_by(CDUID) %>% 
-  summarise(mean_obs_per_observer = mean(nb_obs), nb_observer = n())
+  summarise(mean_obs_per_observer = mean(nb_obs), 
+            nb_observer = n(),
+            stderror = sd(nb_obs)/sqrt(length(nb_obs)))
 
 
 ## 2. Observations per capita (#obs/total_pop)
